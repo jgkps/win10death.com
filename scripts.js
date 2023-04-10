5bcf12d463f8d5ff1d816cf472c82552e7f86e9e
Jóhannes Geir Kristjánsson <30498538+jgkps@users.noreply.github.com>
2023-04-10 21:15:14 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function updateCountdown() {
+    const endDate = new Date("2025-10-14T00:00:00");
+    const currentTime = new Date();
+    const timeDifference = endDate - currentTime;
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = days;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
